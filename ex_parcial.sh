c155199dae8d64d2400f63f57c7d10f279ec0202
osrodas007 <osrodas@gmail.com>
2024-10-29 19:56:36 -0600

    Update ex_parcial.sh

--- ex_parcial.sh
+++ ex_parcial.sh
@@ -14,15 +14,15 @@ ip a show dev ens33
 
 echo "Datos del Sistema"
 echo ""
-snmpwalk -v 1 -c examen_parcial_23 $IP system.1
-snmpwalk -v 1 -c examen_parcial_23 $IP system.3
-snmpwalk -v 1 -c examen_parcial_23 $IP system.4
-snmpwalk -v 1 -c examen_parcial_23 $IP system.5
-snmpwalk -v 1 -c examen_parcial_23 $IP system.6
+snmpwalk -v 1 -c examen_parcial_24 $IP system.1
+snmpwalk -v 1 -c examen_parcial_24 $IP system.3
+snmpwalk -v 1 -c examen_parcial_24 $IP system.4
+snmpwalk -v 1 -c examen_parcial_24 $IP system.5
+snmpwalk -v 1 -c examen_parcial_24 $IP system.6
 
 echo "Otros datos"
 echo ""
-snmpwalk -v 1 -c examen_parcial_23 $IP icmpInMsgs
-snmpwalk -v 1 -c examen_parcial_23 $IP icmpInEchos
-snmpwalk -v 1 -c examen_parcial_23 $IP icmpInEchoReps
-snmpwalk -v 1 -c examen_parcial_23 $IP icmpOutEchoReps
+snmpwalk -v 1 -c examen_parcial_24 $IP icmpInMsgs
+snmpwalk -v 1 -c examen_parcial_24 $IP icmpInEchos
+snmpwalk -v 1 -c examen_parcial_24 $IP icmpInEchoReps
+snmpwalk -v 1 -c examen_parcial_24 $IP icmpOutEchoReps
