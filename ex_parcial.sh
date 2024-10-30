a5c10bb64d5a7571e52e45cd1513433beca679e4
osrodas007 <osrodas@gmail.com>
2024-10-29 19:46:20 -0600

    Create ex_parcial.sh

---
+++ ex_parcial.sh
@@ -0,0 +1,28 @@
+#/bin/sh
+
+#Coloque IP
+IP=192.168.81.128
+
+clear
+
+echo ""
+echo "Revision NMM - PRC 2024"
+
+echo ""
+echo "IP"
+ip a show dev ens33
+
+echo "Datos del Sistema"
+echo ""
+snmpwalk -v 1 -c examen_parcial_23 $IP system.1
+snmpwalk -v 1 -c examen_parcial_23 $IP system.3
+snmpwalk -v 1 -c examen_parcial_23 $IP system.4
+snmpwalk -v 1 -c examen_parcial_23 $IP system.5
+snmpwalk -v 1 -c examen_parcial_23 $IP system.6
+
+echo "Otros datos"
+echo ""
+snmpwalk -v 1 -c examen_parcial_23 $IP icmpInMsgs
+snmpwalk -v 1 -c examen_parcial_23 $IP icmpInEchos
+snmpwalk -v 1 -c examen_parcial_23 $IP icmpInEchoReps
+snmpwalk -v 1 -c examen_parcial_23 $IP icmpOutEchoReps
